15c02e46b0c35ce75a26bca11e9827ba239f9e18
Ivan Oreshnikov <oreshnikov.ivan@gmail.com>
2020-06-11 16:30:39 +0200

    Finally, the theme work on load.

--- init.el
+++ init.el
@@ -1,2 +1,8 @@
+(let ((tangle-directory "~/.emacs.d/tangle"))
+  (when (not (file-directory-p tangle-directory))
+    (make-directory "~/.emacs.d/tangle"))
+  (add-to-list 'load-path tangle-directory)
+  (add-to-list 'custom-theme-load-path tangle-directory))
+
 (org-babel-load-file
  (expand-file-name "README.org" user-emacs-directory))
